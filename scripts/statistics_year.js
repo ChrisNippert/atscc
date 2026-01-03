191d5d7f56ac4e75ab9906024d2f8a96d328f06c
Kmek <kmekeefe@gmail.com>
2026-01-02 20:36:41 -0500

    Display box plots per person per year

--- scripts/statistics_year.js
+++ scripts/statistics_year.js
@@ -20,4 +20,43 @@ else {
     // Show table of data for that year
     pageDiv.innerHTML += `<div class="row text-blob text" id="tables-div"></div>`
     createTable(document.getElementById("tables-div"), requestedYear)
+
+    const yearData = sortedData.allRows.filter(o => o.year == requestedYear)
+    const yearDataValid = yearData.filter(o => o.isNumeric)
+
+    if (yearDataValid.length > 0) {
+        // Plot box plot of all points
+        pageDiv.innerHTML += `<div id="myDiv"></div>`
+        var traceAll = {
+            x: yearDataValid.map(o => o.cut),
+            type: 'box',
+            name: 'All',
+            boxpoints: 'all'
+        };
+        var traceParticipants = sortedData[requestedYear].standings.reverse().map(person => ({
+            x: yearDataValid.filter(o => o.person === person).map(o => o.cut),
+            type: 'box',
+            name: person,
+            boxpoints: 'all'
+        }));
+        var data = [... traceParticipants, traceAll];
+        var layout = {
+            title: {
+                text: `Cut Widths for ${requestedYear}`
+            },
+            xaxis: {
+                title: {
+                    text: 'Cut Width (mm)',
+                },
+                fixedrange: true
+            },
+            yaxis: {
+                fixedrange: true
+            },
+        };
+        Plotly.newPlot('myDiv', data, layout, {
+            displayModeBar: false,
+            scrollZoom: false
+        });
+    }
 }
